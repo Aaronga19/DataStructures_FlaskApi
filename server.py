749f719d6785679c39914370ea957f833a62bbce
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-11-09 12:01:31 -0600

    get numeric bodies and delete 10 last posts

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import relation
 import linked_list
 import hash_table
 import binary_search_tree
+import custom_q
+import stack
 import random
 
 # configure app
@@ -181,14 +183,51 @@ def get_all_blog_posts(blog_post_id):
         return jsonify({"message": "Post not found"})
     return jsonify(post)
 
-@app.route('/blog_post/<blog_post_id>', methods=["GET"])
-def get_one_blog_post(blog_post_id):
-    pass
+@app.route('/blog_post/numeric_body', methods=["GET"])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all()
+    q = custom_q.Queue()
+    for post in blog_posts:
+        q.enqueue(post)
 
-@app.route('/blog_post/<blog_post_id>', methods=["DELETE"])
-def delete_blog_post(blog_post_id):
-    pass
+    return_list = []
 
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+        
+        post.data.body = numeric_body
+
+        return_list.append(
+            {
+                "id": post.data.id,
+                "title": post.data.title,
+                "body" : post.data.body,
+                "date": post.data.date,
+                "user_id" : post.data.user_id
+            }
+        )
+    
+    return jsonify(return_list)
+
+
+
+@app.route('/blog_post/delete_last_10', methods=["DELETE"])
+def delete_last_ten():
+    blog_posts = BlogPost.query.all()
+    s = stack.Stack()
+
+    for post in blog_posts:
+        s.push(post)
+    
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete.data)
+        db.session.commit()
+
+    return jsonify({"message": "Deleted Successed!"})
 
 if __name__== '__main__':
     app.run(debug=True)
